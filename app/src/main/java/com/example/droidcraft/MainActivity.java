e15e4f71c09a2a2675dd00faca4fb3ef7a617b58
davealone69-gif <davealone69@gmail.com>
2026-07-09 19:40:43 +0800

    Autonomous App Build Factory v3.0 - Cycle 1 Attempt 3

--- app/src/main/java/com/example/droidcraft/MainActivity.java
+++ app/src/main/java/com/example/droidcraft/MainActivity.java
@@ -11,8 +11,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView timerText;
-    private Button btnStart, btnColor;
+    private TextView tvTimer, tvHeader;
+    private Button btnStart, btnChangeColor;
     private CountDownTimer countDownTimer;
     private MediaPlayer clickSound;
 
@@ -21,9 +21,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        timerText = findViewById(R.id.timerText);
+        tvTimer = findViewById(R.id.tvTimer);
+        tvHeader = findViewById(R.id.tvHeader);
         btnStart = findViewById(R.id.btnStart);
-        btnColor = findViewById(R.id.btnColor);
+        btnChangeColor = findViewById(R.id.btnChangeColor);
 
         clickSound = MediaPlayer.create(this, R.raw.click_sound);
 
@@ -36,23 +37,24 @@ public class MainActivity extends AppCompatActivity {
                 }
                 countDownTimer = new CountDownTimer(30000, 1000) {
                     public void onTick(long millisUntilFinished) {
-                        timerText.setText("Seconds remaining: " + millisUntilFinished / 1000);
+                        tvTimer.setText("Seconds remaining: " + millisUntilFinished / 1000);
                     }
                     public void onFinish() {
-                        timerText.setText("Time's up!");
+                        tvTimer.setText("Time's up!");
                     }
                 }.start();
             }
         });
 
-        btnColor.setOnClickListener(new View.OnClickListener() {
+        btnChangeColor.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 playSound();
                 Random rnd = new Random();
                 int color = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
-                timerText.setTextColor(color);
+                tvHeader.setTextColor(color);
                 btnStart.setBackgroundColor(color);
+                btnChangeColor.setBackgroundColor(color);
             }
         });
     }
@@ -66,11 +68,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        if (countDownTimer != null) {
-            countDownTimer.cancel();
-        }
         if (clickSound != null) {
             clickSound.release();
+        }
+        if (countDownTimer != null) {
+            countDownTimer.cancel();
         }
     }
 }
