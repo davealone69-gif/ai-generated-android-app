5580498a4943321d8f03fd157588979d8a2b330f
davealone69-gif <davealone69@gmail.com>
2026-07-09 18:58:22 +0800

    Autonomous App Build Factory v3.0 - Cycle 1 Attempt 1

--- app/src/main/java/com/example/droidcraft/MainActivity.java
+++ app/src/main/java/com/example/droidcraft/MainActivity.java
@@ -12,8 +12,7 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     private TextView timerText;
-    private Button btnStartTimer;
-    private Button btnChangeColor;
+    private Button btnStart, btnColor;
     private CountDownTimer countDownTimer;
     private MediaPlayer clickSound;
 
@@ -23,21 +22,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         timerText = findViewById(R.id.timerText);
-        btnStartTimer = findViewById(R.id.btnStartTimer);
-        btnChangeColor = findViewById(R.id.btnChangeColor);
-
+        btnStart = findViewById(R.id.btnStart);
+        btnColor = findViewById(R.id.btnColor);
+        
         clickSound = MediaPlayer.create(this, R.raw.click_sound);
 
-        btnStartTimer.setOnClickListener(new View.OnClickListener() {
+        btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 playSound();
-                if (countDownTimer != null) {
-                    countDownTimer.cancel();
-                }
-                countDownTimer = new CountDownTimer(10000, 1000) {
+                if (countDownTimer != null) countDownTimer.cancel();
+                
+                countDownTimer = new CountDownTimer(30000, 1000) {
                     public void onTick(long millisUntilFinished) {
-                        timerText.setText("Time: " + millisUntilFinished / 1000);
+                        timerText.setText("Seconds remaining: " + millisUntilFinished / 1000);
                     }
                     public void onFinish() {
                         timerText.setText("Done!");
@@ -46,13 +44,14 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        btnChangeColor.setOnClickListener(new View.OnClickListener() {
+        btnColor.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 playSound();
                 Random rnd = new Random();
                 int color = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
                 timerText.setTextColor(color);
+                findViewById(R.id.mainLayout).setBackgroundColor(color + 0x20000000);
             }
         });
     }
@@ -66,11 +65,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        if (countDownTimer != null) {
-            countDownTimer.cancel();
-        }
         if (clickSound != null) {
             clickSound.release();
+            clickSound = null;
+        }
+        if (countDownTimer != null) {
+            countDownTimer.cancel();
         }
     }
 }
