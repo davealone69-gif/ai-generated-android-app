0097cce98a942098cd07c2bb569de74ab37f8011
davealone69-gif <davealone69@gmail.com>
2026-07-09 19:08:46 +0800

    Autonomous App Build Factory v3.0 - Cycle 1 Attempt 2

--- app/src/main/java/com/example/droidcraft/MainActivity.java
+++ app/src/main/java/com/example/droidcraft/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
         btnStart = findViewById(R.id.btnStart);
         btnColor = findViewById(R.id.btnColor);
 
-        clickSound = MediaPlayer.create(this, android.R.raw.click);
+        clickSound = MediaPlayer.create(this, R.raw.click_sound);
 
         btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
                 }
                 countDownTimer = new CountDownTimer(30000, 1000) {
                     public void onTick(long millisUntilFinished) {
-                        timerText.setText("Seconds remaining: " + millisUntilFinished / 1000);
+                        timerText.setText("Time: " + millisUntilFinished / 1000);
                     }
                     public void onFinish() {
                         timerText.setText("Done!");
