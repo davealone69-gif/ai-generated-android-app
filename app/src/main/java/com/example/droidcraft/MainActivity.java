659f6c1b843fee793c597daec58cc346b880b327
davealone69-gif <davealone69@gmail.com>
2026-07-09 17:38:36 +0800

    Autonomous App Build Factory v3.0 - Cycle 1 Attempt 1

--- app/src/main/java/com/example/droidcraft/MainActivity.java
+++ app/src/main/java/com/example/droidcraft/MainActivity.java
@@ -11,7 +11,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView timerText;
+    private TextView tvTimer;
     private Button btnStart, btnColor;
     private CountDownTimer countDownTimer;
     private MediaPlayer clickSound;
@@ -21,10 +21,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        timerText = findViewById(R.id.timerText);
+        tvTimer = findViewById(R.id.tvTimer);
         btnStart = findViewById(R.id.btnStart);
         btnColor = findViewById(R.id.btnColor);
-        
+
         clickSound = MediaPlayer.create(this, R.raw.click_sound);
 
         btnStart.setOnClickListener(new View.OnClickListener() {
@@ -35,10 +35,10 @@ public class MainActivity extends AppCompatActivity {
                 
                 countDownTimer = new CountDownTimer(30000, 1000) {
                     public void onTick(long millisUntilFinished) {
-                        timerText.setText("Time: " + millisUntilFinished / 1000);
+                        tvTimer.setText("Time: " + millisUntilFinished / 1000);
                     }
                     public void onFinish() {
-                        timerText.setText("Done!");
+                        tvTimer.setText("Done!");
                     }
                 }.start();
             }
@@ -50,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
                 playSound();
                 Random rnd = new Random();
                 int color = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
-                timerText.setTextColor(color);
+                tvTimer.setTextColor(color);
             }
         });
     }
